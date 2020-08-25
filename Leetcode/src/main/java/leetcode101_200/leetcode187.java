611844f75b30be5006375c65cf65ab357331fd61
songny <songyuehao@qq.com>
2020-08-25 22:12:56 +0800

    187重复DNA序列,使用hashSet

---
+++ Leetcode/src/main/java/leetcode101_200/leetcode187.java
@@ -0,0 +1,29 @@
+package leetcode101_200;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class leetcode187 {
+    public static void main(String[] args){
+        String strs = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+        List<String> strList = findRepeatedDnaSequences(strs);
+        for (String str:strList){
+            System.out.print(str+",");
+        }
+    }
+    public static List<String> findRepeatedDnaSequences(String s){
+        int lenght = 10;
+        Set<String> input = new HashSet();
+        List<String> result = new ArrayList<String>();
+        for (int i =0;i<s.length()-lenght;i++){
+            String currentStr = s.substring(i,i+10);
+            if (input.contains(currentStr)){
+                result.add(currentStr);
+            }
+            input.add(currentStr);
+        }
+        return result;
+    }
+}
